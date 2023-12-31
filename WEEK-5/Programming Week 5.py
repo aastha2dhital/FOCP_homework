4ad3be934f48c5f19839e798fa4fa3b7dc33a530
aastha2dhital <aastha2dhital@gmail.com>
2023-12-31 09:42:47 +0545

    Week 7 Assignment

---
+++ WEEK-5/Programming Week 5.py
@@ -0,0 +1,17 @@
+# Qno.1 
+import sys
+def report_os_platform():
+    platform = sys.platform
+    print(f"Operating System Platform: {platform}")
+if __name__ == "__main__":
+    report_os_platform()
+
+# Qno.2
+import sys
+def main():
+    arguments_num = len(sys.argv) - 1
+    print(f"Number of arguments passed: {arguments_num}")
+if __name__ == "__main__":
+    main()
+
+# Qno.3 
